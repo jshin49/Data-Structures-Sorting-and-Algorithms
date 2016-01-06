635947e16fea87fd11bdecafbe2c2c985f1a31f0
jshin49@gmail.com <jshin49@gmail.com>
2016-01-06 05:51:28 +0100

    queue.hpp interface

--- Data Structures/queue.hpp
+++ Data Structures/queue.hpp
@@ -6,8 +6,42 @@
 #include <algorithm>
 
 class Queue {
+
+	class Node {
+	public:
+		//Constructors
+		Node(){}
+		Node(Node* next) : next_m(next){}
+		Node(int elem, Node* next) : elem_m(elem), next_m(next){}
+
+		// Getter
+		Node* next() { return next_m; }
+		int elem() { return elem_m; }
+
+		// Setter
+		void set_next(Node* next) { next_m = next; }
+		void set_elem(int elem) { elem_m = elem; }
+
+	private:
+		Node* next_m;
+		int elem_m;
+	};
+
+	// Sentinel Node
+	class Nil : public Node {};
+
 public:
+	// Constructors
+
+	// Insert
+
+	// Remove
+
+	// is_empty
+
 private:
+	Node* head_m;
+	int size_m;
 
 };
 
